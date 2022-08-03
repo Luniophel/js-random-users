22b75c74477dbd818f5de6c0e6ccce499a140184
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-08-03 16:03:44 +0200

    tested btn onClick function by js

---
+++ axios-user.js
@@ -0,0 +1,14 @@
+//Chiamata su API
+const addBtn = document.getElementById('add');
+console.log(addBtn);
+addBtn.addEventListener('click', addUser);
+
+//Ricava img profilo, nome, cognome
+function addUser()
+{
+    console.log('bottone linkato ad html')
+}
+
+//Inseriscili su card
+
+//
