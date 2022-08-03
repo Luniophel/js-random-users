78927a6ce70c2ba840f0bbd1a6f2a518fb724027
Luniophel <lucarello.stefanoemanuele@gmail.com>
2022-08-03 17:20:43 +0200

    add userData function

--- axios-user.js
+++ axios-user.js
@@ -6,7 +6,24 @@ addBtn.addEventListener('click', addUser);
 //Ricava img profilo, nome, cognome
 function addUser()
 {
-    console.log('bottone linkato ad html')
+    //console.log('bottone linkato ad html')
+    axios.get('https://randomuser.me/api')
+        .then
+        (
+            function (response)
+            {
+                const userData = response.data.results[0];
+                
+                const userName = userData.name.first;
+                const userSurname = userData.name.last;
+                const userPic = userData.picture.thumbnail;
+
+            }
+        )
+        .catch(function(err)
+        {
+            console.log(err);
+        });
 }
 
 //Inseriscili su card
